97770ceddee1fde8dfe7f006ff1836a5218b03c9
wilyCesde <wruizza@cesde.net>
2023-04-15 09:55:58 -0500

    let email = 'cosas@gmail.com';   let phone = '604356789';

--- screens/ContactScreen.jsx
+++ screens/ContactScreen.jsx
@@ -1,12 +1,17 @@
 
 
 import { View, Text, Button, } from 'react-native'
-import {styles} from '../assets/style/styles'
+import { styles } from '../assets/style/styles'
 
-export default function ContactScreen() {
+export default function ContactScreen({ route }) {
+
+  const { email, phone } = route.params   //desestructurado
+
   return (
     <View style={styles.container}>
       <Text style={{ fontWeight: 'bold' }}>Estamos en Contact</Text>
+      <Text>Email:{route.params.email}</Text>
+      <Text>Telefono:{phone}</Text>
     </View>
   )
 }
--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,12 +1,22 @@
 
 import { View, Text, Button, } from 'react-native'
-import {styles} from '../assets/style/styles'
+import { styles } from '../assets/style/styles'
 
 
-export default function ProfileScreen() {
+export default function ProfileScreen({navigation}) {
+
+  let email = 'cosas@gmail.com';
+  let phone = '604356789';
+
   return (
     <View style={styles.container}>
-      <Text style={{ fontWeight: 'bold' }}>Estamos en Profile</Text>
+      <Text style={{ fontWeight: 'bold', marginBottom: 10 }}>Estamos en el perfil del usuario</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate('Contacts', { email: email, phone: phone })
+        }}
+        title="Contáctenos"
+      />
     </View>
   )
 }
