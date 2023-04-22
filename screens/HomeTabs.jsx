75d0c7c0dfe01212719b7d2e2d1ea573beda8759
wilyCesde <wruizza@cesde.net>
2023-04-22 09:26:41 -0500

    HomeTabs

--- screens/HomeTabs.jsx
+++ screens/HomeTabs.jsx
@@ -1,21 +1,36 @@
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from'./ContactScreen';
+import { MaterialIcons } from '@expo/vector-icons'//iconos material desing
+import HomeScreen from './ContactScreen';
 import ProfileScreen from "./ProfileScreen";
 import ContactScreen from "./ContactScreen";
+
+
 const tab = createBottomTabNavigator();
 
 //esto es un cintenedor tab.Navigator
 export default function HomeTabs() {
   return (
     <tab.Navigator
-    
-    initialRouteName="Home"
-    screenOptions={{headerShown:false}}
+
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: 'red',
+        tabBarInactiveTintColor: 'green',
+        tabBarActiveBackgroundColor: 'orange',
+        tabBarInactiveBackgroundColor: 'gray'
+      }}
     >
-      <tab.Screen name="Home" component={HomeScreen} />
-      <tab.Screen name="Prorofile" component={ProfileScreen} />
-      <tab.Screen name="Contacts" component={ContactScreen} />
+      <tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarIcon: () => (<MaterialIcons name='home' size={22} />)
+      }} />
+      <tab.Screen name="Prorofile" component={ProfileScreen} options={{
+        tabBarIcon: () => (<MaterialIcons name='person' size={22} />)
+      }} />
+      <tab.Screen name="Contacts" component={ContactScreen} options={{
+        tabBarIcon: () => (<MaterialIcons name='phone' size={22} />)
+      }} />
     </tab.Navigator>
   );
 }
